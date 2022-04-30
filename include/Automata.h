9890c9081c94f66fcb37b8976cdce6995bbd76cb
ThatsNotSo <104660360+ThatsNotSo@users.noreply.github.com>
2022-04-30 09:15:52 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,22 +1,20 @@
-
 // Copyright 2022 UNN-IASR
 #pragma once
-
 #include <iostream>
 #include <string>
 
 using std::string;
 
 class Automata {
-private:
+ private:
     enum State { OFF, WAIT, ACCEPT, CHECK, COOK };
     double _amount = 0;
-    string menu[2] = { "Coffee" , "Tea" };
+    string menu[2] = { "Tea" , "Coffee" };
     int prices[2] = { 100, 200 };
     State state = OFF;
     int _selectedMenu;
 
-public:
+ public:
     void On();
     void Off();
     void AddCoin(int money);
